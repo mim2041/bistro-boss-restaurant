8bdbfc7681d1d580829946e1583134d5e7e3a21b
mim2041 <suraiyamim338@gmail.com>
2023-06-04 23:05:43 +0600

    admin updated

--- index.js
+++ index.js
@@ -52,6 +52,18 @@ async function run() {
       res.send(result);
     })
 
+    app.patch('/users/admin/:id', async(req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await usersCollection.updateOne(filter, updatedDoc);
+      res.send(result);
+    })
+
     app.get("/menu", async(req, res) => {
       const result = await menuCollection.find().toArray();
       res.send(result);
